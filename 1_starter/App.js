c8cf0b13dad96a55559145485b5ef4ef35a878f9
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-23 15:56:58 +0300

    first project added.
    first project added.

---
+++ 1_starter/App.js
@@ -0,0 +1,19 @@
+
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+
+import HomeScreen from './src/screens/HomeScreen';
+
+const navigator = createStackNavigator (
+  {
+    Home: HomeScreen,
+  },
+  {
+    initialRouteName: 'Home',
+    defaultNavigationOptions: {
+      title: 'App',
+    },
+  }
+);
+
+export default createAppContainer (navigator);
