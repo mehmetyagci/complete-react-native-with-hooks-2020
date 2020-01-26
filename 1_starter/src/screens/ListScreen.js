be301779dcdbe1a4fa2f1294d3c9821a7d5a23cb
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-26 10:20:20 +0300

    21. Solving the Key Issue
    21. Solving the Key Issue

--- 1_starter/src/screens/ListScreen.js
+++ 1_starter/src/screens/ListScreen.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 
 const ListScreen = () => {
-  return <Text>List Screen</Text>;
+  const friends = [
+    {name: 'Friend #1'},
+    {name: 'Friend #2'},
+    {name: 'Friend #3'},
+    {name: 'Friend #4'},
+    {name: 'Friend #5'},
+    {name: 'Friend #6'},
+    {name: 'Friend #7'},
+    {name: 'Friend #8'},
+    {name: 'Friend #9'},
+    {name: 'Friend #10'},
+    {name: 'Friend #11'},
+    {name: 'Friend #12'},
+  ];
+
+  return (
+    <FlatList
+      keyExtractor={friend => friend.name}
+      data={friends}
+      renderItem={({item}) => {
+        return <Text>{item.name}</Text>;
+        // item === { name: 'Friend #1' }
+        // element === {item: name:'Friend #1'}, index:0 }
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create ({});
---
+++ 1_starter/src/screens/ListScreen2.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+
+const ListScreen2 = () => {
+  const friends = [
+    {name: 'Friend #1', key: '1'},
+    {name: 'Friend #2', key: '2'},
+    {name: 'Friend #3', key: '3'},
+    {name: 'Friend #4', key: '4'},
+    {name: 'Friend #5', key: '5'},
+    {name: 'Friend #6', key: '6'},
+    {name: 'Friend #7', key: '7'},
+    {name: 'Friend #8', key: '8'},
+    {name: 'Friend #9', key: '9'},
+    {name: 'Friend #10', key: '10'},
+    {name: 'Friend #11', key: '11'},
+    {name: 'Friend #12', key: '12'},
+  ];
+
+  return (
+    <FlatList
+      data={friends}
+      renderItem={({item}) => {
+        return <Text>{item.name}</Text>;
+        // item === { name: 'Friend #1' }
+        // element === {item: name:'Friend #1'}, index:0 }
+      }}
+    />
+  );
+};
+
+const styles = StyleSheet.create ({});
+
+export default ListScreen2;
