b728d3c10492ec88d559f7673f720e1a2bd6f9ce
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-27 08:11:52 +0300

    32. Exercise Solution
    32. Exercise Solution

--- 1_starter/src/screens/HomeScreen.js
+++ 1_starter/src/screens/HomeScreen.js
@@ -14,6 +14,10 @@ const HomeScreen = ({navigation}) => {
         title="Go to List Demo"
         onPress={() => navigation.navigate ('List')}
       />
+      <Button
+        title="Go to Images"
+        onPress={() => navigation.navigate ('Image')}
+      />
     </View>
   );
 };
---
+++ 1_starter/src/screens/ImageScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const ImageScreen = () => {
+  return (
+    <View>
+      <Text>ImageScreen</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create ({});
+
+export default ImageScreen;
