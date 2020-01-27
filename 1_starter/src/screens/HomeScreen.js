2e7b68e05b2e2392693317ad8a1c311ed8001185
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-27 07:34:23 +0300

    29. Navigating with React Navigation

--- 1_starter/src/screens/HomeScreen.js
+++ 1_starter/src/screens/HomeScreen.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Text, StyleSheet, View, Button, TouchableOpacity} from 'react-native';
 
-const HomeScreen = () => {
+const HomeScreen = props => {
+  console.log ('HomeScreen->props.navigation', props.navigation);
   return (
     <View>
       <Text style={styles.text}>Hi MY! Work harder and clever.</Text>
       <Button
-        onPress={() => console.log ('Button pressed')}
+        onPress={() => props.navigation.navigate ('Components')}
         title="Go to Components Demo"
       />
       <TouchableOpacity onPress={() => console.log ('TouchableOpacity presed')}>
