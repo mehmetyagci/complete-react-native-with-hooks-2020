eb4b1a1e35888a0384108eaad0adb3e21bce5fe6
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-27 07:29:07 +0300

    27. Touchable Opacity in Action

--- 1_starter/src/screens/HomeScreen.js
+++ 1_starter/src/screens/HomeScreen.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View, Button, TouchableOpacity} from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hi MY! Work harder and clever.</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Hi MY! Work harder and clever.</Text>
+      <Button
+        onPress={() => console.log ('Button pressed')}
+        title="Go to Components Demo"
+      />
+      <TouchableOpacity onPress={() => console.log ('TouchableOpacity presed')}>
+        <Text>Go To List Demo</Text>
+        <Text>Go To List Demo</Text>
+        <Text>Go To List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create ({
