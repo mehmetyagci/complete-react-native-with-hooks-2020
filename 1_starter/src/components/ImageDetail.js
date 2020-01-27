584d07d46332fb428bad000fb2083bac8c4cbd0e
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-27 09:08:21 +0300

    section 5 completed!
    section 5 completed!

---
+++ 1_starter/src/components/ImageDetail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image} from 'react-native';
+
+const ImageDetail = ({imageSource, title, score}) => {
+  //console.log ('ImageDetail->props', props);
+  return (
+    <View>
+      <Image source={imageSource} />
+      <Text>{title}</Text>
+      <Text>Image Score - {score}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create ({});
+
+export default ImageDetail;
--- 1_starter/src/screens/ImageScreen.js
+++ 1_starter/src/screens/ImageScreen.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
+import ImageDetail from '../components/ImageDetail';
+
 const ImageScreen = () => {
   return (
     <View>
-      <Text>ImageScreen</Text>
+      <ImageDetail
+        title="Forest"
+        imageSource={require ('../../assets/forest.jpg')}
+        score={9}
+      />
+      <ImageDetail
+        title="Beach"
+        imageSource={require ('../../assets/beach.jpg')}
+        score={7}
+      />
+      <ImageDetail
+        title="Mountain"
+        imageSource={require ('../../assets/mountain.jpg')}
+        score={10}
+      />
     </View>
   );
 };
