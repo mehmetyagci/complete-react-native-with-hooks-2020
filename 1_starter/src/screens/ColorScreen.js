06d5daaf384e60077b3afe1ec24d8c46c796605a
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-28 11:56:17 +0300

    47. Showing Colors with a FlatList

--- 1_starter/src/screens/ColorScreen.js
+++ 1_starter/src/screens/ColorScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, Button} from 'react-native';
+import {View, StyleSheet, Button, FlatList} from 'react-native';
 
 const ColorScreen = () => {
   const [colors, setColors] = useState ([]);
@@ -14,7 +14,18 @@ const ColorScreen = () => {
           setColors ([...colors, randomRgb ()]);
         }}
       />
-      <View style={{height: 100, width: 100, backgroundColor: randomRgb ()}} />
+
+      <FlatList
+        keyExtractor={item => item}
+        data={colors}
+        renderItem={({item}) => {
+          console.log ('color item:', item);
+          // item === 'rgb(0,0,0)'
+          return (
+            <View style={{height: 100, width: 100, backgroundColor: item}} />
+          );
+        }}
+      />
     </View>
   );
 };
