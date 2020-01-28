0fe67bfa828fa96e276ba4ae31e0a51e9c8a9a29
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-28 11:41:08 +0300

    44. App Overview

--- 1_starter/src/screens/HomeScreen.js
+++ 1_starter/src/screens/HomeScreen.js
@@ -22,6 +22,10 @@ const HomeScreen = ({navigation}) => {
         title="Go to Counter"
         onPress={() => navigation.navigate ('Counter')}
       />
+      <Button
+        title="Go to Color"
+        onPress={() => navigation.navigate ('Color')}
+      />
     </View>
   );
 };
