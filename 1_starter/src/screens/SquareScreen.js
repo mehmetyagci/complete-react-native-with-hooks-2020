e1ec33046e36c8066eade61bccc12381807bc44a
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-28 12:33:08 +0300

    49. Reusable Color Adjusters
    49. Reusable Color Adjusters

--- 1_starter/src/screens/SquareScreen.js
+++ 1_starter/src/screens/SquareScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
+import ColorCounter from '../components/ColorCounter';
+
 const SquareScreen = () => {
   return (
     <View>
-      <Text>Square Screen</Text>
+      <ColorCounter color="Red" />
+      <ColorCounter color="Blue" />
+      <ColorCounter color="Green" />
     </View>
   );
 };
