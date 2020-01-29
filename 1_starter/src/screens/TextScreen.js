548c104851238ba44ae1e896d91881c3abf32483
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-29 10:49:17 +0300

    66. Updating State
    66. Updating State

--- 1_starter/src/screens/TextScreen.js
+++ 1_starter/src/screens/TextScreen.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
 
 const TextScreen = () => {
+  const [name, setName] = useState ('');
+
   return (
     <View>
-      <Text>Text Screen</Text>
+      <Text>Enter Name:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={name}
+        onChangeText={newValue => setName (newValue)}
+      />
+      <Text>My name is:{name}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create ({});
+const styles = StyleSheet.create ({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+});
 
 export default TextScreen;
