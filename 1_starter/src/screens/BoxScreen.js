281228df4af2534337309c7476091dcc1cbc5ea0
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-30 08:08:37 +0300

    74

--- 1_starter/src/screens/BoxScreen.js
+++ 1_starter/src/screens/BoxScreen.js
@@ -4,9 +4,9 @@ import {Text, View, StyleSheet} from 'react-native';
 const BoxScreen = () => {
   return (
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>Child #1</Text>
-      <Text style={styles.textStyle}>Child #2</Text>
-      <Text style={styles.textStyle}>Child #3</Text>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
     </View>
   );
 };
@@ -16,11 +16,20 @@ const styles = StyleSheet.create ({
     borderWidth: 3,
     borderColor: 'black',
     height: 200,
-    justifyContent: 'center',
   },
-  textStyle: {
+  textOneStyle: {
     borderWidth: 3,
     borderColor: 'red',
+  },
+  textTwoStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    flex: 1,
+  },
+  textThreeStyle: {
+    borderWidth: 3,
+    borderColor: 'red',
+    flex: 1,
   },
 });
 
