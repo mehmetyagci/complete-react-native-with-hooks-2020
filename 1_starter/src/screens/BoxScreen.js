885b54c4a8f6d697f206ff62e7f6e2f6de2d0f8e
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-30 07:26:25 +0300

    71

--- 1_starter/src/screens/BoxScreen.js
+++ 1_starter/src/screens/BoxScreen.js
@@ -4,7 +4,10 @@ import {Text, View, StyleSheet} from 'react-native';
 const BoxScreen = () => {
   return (
     <View stlye={styles.viewStyle}>
-      <Text style={styles.textStyle}>Box Screen</Text>
+      <Text style={styles.textStyle}>Child #1</Text>
+      <Text style={styles.textStyle}>Child #2</Text>
+      <Text style={styles.textStyle}>Child #3</Text>
+
     </View>
   );
 };
@@ -13,12 +16,11 @@ const styles = StyleSheet.create ({
   viewStyle: {
     borderWidth: 3,
     bordercolor: 'black',
+    alignItems: "stretch"  // default value
   },
   textStyle: {
     borderWidth: 2,
     borderColor: 'red',
-    marginVertical: 20,
-    marginHorizontal: 30,
   },
 });
 
