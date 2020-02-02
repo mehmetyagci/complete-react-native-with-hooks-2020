3595278045ddf4eacd076abcc8548ee44d9a20c1
mehmet yagci <mehmetyagci53@hotmail.com>
2020-02-02 22:41:47 +0300

    95. Detecting Editing Completion
    95. Detecting Editing Completion

--- 2_food/src/screens/SearchScreen.js
+++ 2_food/src/screens/SearchScreen.js
@@ -8,7 +8,11 @@ const SearchScreen = () => {
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={newTerm => setTerm (newTerm)} />
+      <SearchBar
+        term={term}
+        onTermChange={newTerm => setTerm (newTerm)}
+        onTermSubmit={() => console.log ('term was submitted')}
+      />
       <Text>Search Screen</Text>
       <Text> {term} </Text>
     </View>
