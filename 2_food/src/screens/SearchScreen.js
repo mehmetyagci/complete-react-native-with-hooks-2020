e79182d79a41dbeaf7c491e4f59df09b3fdf3767
mehmet yagci <mehmetyagci53@hotmail.com>
2020-02-02 22:35:49 +0300

    94. Managing State
    94. Managing State

--- 2_food/src/screens/SearchScreen.js
+++ 2_food/src/screens/SearchScreen.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState ('');
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar term={term} onTermChange={newTerm => setTerm (newTerm)} />
       <Text>Search Screen</Text>
+      <Text> {term} </Text>
     </View>
   );
 };
