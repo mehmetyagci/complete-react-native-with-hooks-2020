e6bcb33b007c8d5fff7dda04ee3f7919702daa69
mehmet yagci <mehmetyagci53@hotmail.com>
2020-02-03 19:08:38 +0300

    101. Extracting Hook Logic
    101. Extracting Hook Logic

---
+++ 2_food/src/hooks/useResults.js
@@ -0,0 +1,46 @@
+import {useState, useEffect} from 'react';
+
+import yelp from '../api/yelp';
+
+export default () => {
+  const [results, setResults] = useState ([]);
+  const [errorMessage, setErrorMessage] = useState ('');
+
+  const searchApi = async searchTerm => {
+    console.log ('Hi there!');
+    try {
+      //console.log ('searchApi');
+      const response = await yelp.get ('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'istanbul',
+        },
+      });
+      //console.log ('response2:', response);
+      //console.log ('response.data2:', response.data);
+      //console.log ('response.data.businesses2:', response.data.businesses);
+      setResults (Object.values (response.data.businesses));
+      // console.log (
+      //   'response.data.businesses->keys:',
+      //   Object.keys (response.data.businesses)
+      // );
+      // console.log (
+      //   'response.data.businesses->values:',
+      //   Object.values (response.data.businesses)
+      // );
+    } catch (err) {
+      console.log (err);
+      setErrorMessage ('Something went wrong');
+    }
+  };
+
+  // Call searchApi when component
+  // is first rendered. BAD CODE!
+  // searchApi ('pasta');
+  useEffect (() => {
+    searchApi ('pasta');
+  }, []);
+
+  return [searchApi, results, errorMessage];
+};
--- 2_food/src/screens/SearchScreen.js
+++ 2_food/src/screens/SearchScreen.js
@@ -2,45 +2,11 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState ('');
-  const [results, setResults] = useState ([]);
-  const [errorMessage, setErrorMessage] = useState ('');
-
-  const searchApi = async searchTerm => {
-    console.log ('Hi there!');
-    try {
-      //console.log ('searchApi');
-      const response = await yelp.get ('/search', {
-        params: {
-          limit: 2,
-          term: searchTerm,
-          location: 'istanbul',
-        },
-      });
-      //console.log ('response2:', response);
-      //console.log ('response.data2:', response.data);
-      //console.log ('response.data.businesses2:', response.data.businesses);
-      setResults (Object.keys (response.data.businesses).length);
-      // console.log (
-      //   'response.data.businesses->keys:',
-      //   Object.keys (response.data.businesses)
-      // );
-      // console.log (
-      //   'response.data.businesses->values:',
-      //   Object.values (response.data.businesses)
-      // );
-    } catch (err) {
-      console.log (err);
-      setErrorMessage ('Something went wrong');
-    }
-  };
-
-  // Call searchApi when component
-  // is first rendered. BAD CODE!
-  // searchApi ('pasta');
+  const [searchApi, results, errorMessage] = useResults ();
 
   return (
     <View>
@@ -50,9 +16,9 @@ const SearchScreen = () => {
         onTermSubmit={() => searchApi (term)}
       />
       {/* onTermSubmit={() => searchApi ()} */}
-      <Text>Search Screen</Text>
-      <Text> {term} results: </Text>
-      <Text> We have found {results} results </Text>
+      {/* <Text>Search Screen</Text> */}
+      {/* <Text> {term} results: </Text> */}
+      <Text> We have found {results.length} results </Text>
       {errorMessage ? <Text>ErrorMessage: {errorMessage}</Text> : null}
     </View>
   );
