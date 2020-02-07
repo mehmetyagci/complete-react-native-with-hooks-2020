7d365163932d00c669604fe96a0d584738ce05cc
mehmet yagci <mehmetyagci53@hotmail.com>
2020-02-07 14:38:46 +0300

    sad

--- 3_blog/src/screens/IndexScreen.js
+++ 3_blog/src/screens/IndexScreen.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useContext} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+  const blogPosts = useContext (BlogContext);
+
   return (
     <View>
       <Text>Index Screen</Text>
+      <FlatList
+        style={styles.flatListStyle}
+        data={blogPosts}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({item}) => {
+          return <Text style={styles.blogPostStyle}>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create ({});
+const styles = StyleSheet.create ({
+  flatListStyle: {
+    backgroundColor: '#f5f5f5',
+    color: 'blue',
+  },
+  blogPostStyle: {
+    margin: 10,
+    color: 'blue',
+    padding: 5,
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+  },
+});
 
 export default IndexScreen;
