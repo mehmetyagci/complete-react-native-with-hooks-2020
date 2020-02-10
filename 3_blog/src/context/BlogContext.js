5442641dd7fdbfc058b89d741eab0a4d20b06bbf
mehmet yagci <mehmetyagci53@hotmail.com>
2020-02-10 15:49:23 +0300

    dsf
    sdfsdf

--- 3_blog/src/context/BlogContext.js
+++ 3_blog/src/context/BlogContext.js
@@ -1,6 +1,5 @@
-import React, {useState, useReducer} from 'react';
-
-const BlogContext = React.createContext ();
+import React, {useReducer} from 'react';
+import createDataContext from './createDataContext';
 
 /* state or blogPosts */
 const blogReducer = (state, action) => {
@@ -12,19 +11,16 @@ const blogReducer = (state, action) => {
   }
 };
 
-export const BlogProvider = ({children}) => {
-  //const blogPosts = [{title: 'Blog Post #1'}, {title: 'Blog Post #2'}];
-  const [blogPosts, dispatch] = useReducer (blogReducer, []);
-
-  const addBlogPost = () => {
+const addBlogPost = dispatch => {
+  return () => {
     dispatch ({type: 'add_blogpost'});
   };
-
-  return (
-    <BlogContext.Provider value={{data: blogPosts, addBlogPost: addBlogPost}}>
-      {children}
-    </BlogContext.Provider>
-  );
 };
 
-export default BlogContext;
+export const {Context, Provider} = createDataContext (
+  blogReducer,
+  {
+    addBlogPost,
+  },
+  []
+);
--- 3_blog/src/screens/IndexScreen.js
+++ 3_blog/src/screens/IndexScreen.js
@@ -1,9 +1,9 @@
 import React, {useContext} from 'react';
 import {View, Text, StyleSheet, FlatList, Button} from 'react-native';
-import BlogContext from '../context/BlogContext';
+import {Context} from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const {data, addBlogPost} = useContext (BlogContext);
+  const {state, addBlogPost} = useContext (Context);
 
   return (
     <View>
@@ -11,7 +11,7 @@ const IndexScreen = () => {
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         style={styles.flatListStyle}
-        data={data}
+        data={state}
         keyExtractor={blogPost => blogPost.title}
         renderItem={({item}) => {
           return <Text style={styles.blogPostStyle}>{item.title}</Text>;
