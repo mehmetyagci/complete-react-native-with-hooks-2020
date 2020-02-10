98614315b68cc620d53006a05e252c6ca317b1df
mehmet yagci <mehmetyagci53@hotmail.com>
2020-02-10 12:19:04 +0300

    128. Adding State in Context
    128. Adding State in Context

--- 3_blog/src/context/BlogContext.js
+++ 3_blog/src/context/BlogContext.js
@@ -3,10 +3,18 @@ import React from 'react';
 const BlogContext = React.createContext ();
 
 export const BlogProvider = ({children}) => {
-  const blogPosts = [{title: 'Blog Post #1'}, {title: 'Blog Post #2'}];
+  //const blogPosts = [{title: 'Blog Post #1'}, {title: 'Blog Post #2'}];
+  const [blogPosts, setBlogPosts] = useState ([]);
+
+  const addBlogPost = () => {
+    setBlogPosts ([
+      ...blogPosts,
+      {title: `My blogpost #${blogPosts.length + 1}`},
+    ]);
+  };
 
   return (
-    <BlogContext.Provider value={blogPosts}>
+    <BlogContext.Provider value={{data: blogPosts, addBlogPost: addBlogPost}}>
       {children}
     </BlogContext.Provider>
   );
